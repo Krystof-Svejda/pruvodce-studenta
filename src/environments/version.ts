9cecefe1b63e77800775f976af8895444f8d5f74
krystof <krystof.svejda@centrum.cz>
2020-11-06 19:56:18 +0100

    úprava README

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,26 +2,13 @@
 /* tslint:disable */
 export const version = {
     "dirty": false,
-    "raw": "v0.0.3-0-gc9dc0d4",
-    "hash": "gc9dc0d4",
-    "distance": 0,
-    "tag": "v0.0.3",
-    "semver": {
-        "options": {
-            "loose": false,
-            "includePrerelease": false
-        },
-        "loose": false,
-        "raw": "v0.0.3",
-        "major": 0,
-        "minor": 0,
-        "patch": 3,
-        "prerelease": [],
-        "build": [],
-        "version": "0.0.3"
-    },
-    "suffix": "0-gc9dc0d4",
-    "semverString": "0.0.3",
+    "raw": "6d1a81b",
+    "hash": "6d1a81b",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "6d1a81b",
+    "semverString": null,
     "version": "0.0.3"
 };
 /* tslint:enable */
